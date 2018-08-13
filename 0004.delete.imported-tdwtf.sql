b2eba897f285f28967b4f0814118ee0e5770af5d
Ben Lubar <ben.lubar@gmail.com>
2018-08-13 07:36:39 -0500

    Fix "old_id"

--- 0004.delete.imported-tdwtf.sql
+++ 0004.delete.imported-tdwtf.sql
@@ -8,7 +8,7 @@ DELETE FROM "classify"."unclassified" uc
  USING "classify"."imported_categories" ic
  WHERE uc."_key" = '_imported:_categories'
    AND uc."type" = 'zset'
-   AND uc."unique_string" = ic."old_id"
+   AND uc."unique_string" = ic."discourse_id"
    AND uc."value_numeric" = ic."cid"::NUMERIC;
 
 DELETE FROM "classify"."unclassified" uc
