13975c476e20c376803d9e72c6f83c11b9408221
Ben Lubar <ben.lubar@gmail.com>
2018-09-03 18:27:45 -0500

    Fix nonexistent posts.votes being referenced.

--- 0006.delete.posts.tidpostsvotes.sql
+++ 0006.delete.posts.tidpostsvotes.sql
@@ -3,4 +3,4 @@ DELETE FROM "classify"."unclassified" uc
  WHERE uc."_key" = 'tid:' || p."tid" || ':posts:votes'
    AND uc."type" = 'zset'
    AND uc."unique_string" = p."pid"::TEXT
-   AND uc."value_numeric" = p."votes"::NUMERIC;
+   AND uc."value_numeric" = (p."upvotes" - p."downvotes")::NUMERIC;
