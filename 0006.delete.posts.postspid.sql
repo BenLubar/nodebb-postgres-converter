8813627e18dcedee89c8a43f6fdbfd430e1e3ea5
Ben Lubar <ben.lubar@gmail.com>
2018-09-03 23:08:53 -0500

    Fix post zset deletion.

--- 0006.delete.posts.postspid.sql
+++ 0006.delete.posts.postspid.sql
@@ -3,4 +3,4 @@ DELETE FROM "classify"."unclassified" uc
  WHERE uc."_key" = 'posts:pid'
    AND uc."type" = 'zset'
    AND uc."unique_string" = p."pid"::TEXT
-   AND uc."value_numeric" = (EXTRACT(EPOCH FROM p."timestamp") / 1000)::NUMERIC;
+   AND uc."value_numeric" = (EXTRACT(EPOCH FROM p."timestamp") * 1000)::NUMERIC;
