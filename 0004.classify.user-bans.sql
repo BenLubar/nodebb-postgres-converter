d8d0a2498f383ce77497b68e2d07cddf9e92e89d
Ben Lubar <ben.lubar@gmail.com>
2018-09-03 22:43:38 -0500

    Improve user ban classification.
    Apparently there are some users banned until an absurdly far-away timestamp.

--- 0004.classify.user-bans.sql
+++ 0004.classify.user-bans.sql
@@ -8,20 +8,33 @@ CREATE TABLE "classify"."user_bans" (
 
 INSERT INTO "classify"."user_bans"
 SELECT NEXTVAL('classify.user_bans_ban_id_seq'::REGCLASS),
-       SPLIT_PART(COALESCE(ban."_key", reason."_key"), ':', 2)::BIGINT,
-       TO_TIMESTAMP(COALESCE(ban."value_numeric", reason."value_numeric") / 1000),
-       TO_TIMESTAMP(NULLIF(ban."unique_string", '0')::NUMERIC / 1000),
-       NULLIF(reason."unique_string", '')
-  FROM "classify"."unclassified" ban
-  FULL OUTER JOIN "classify"."unclassified" reason
-               ON SPLIT_PART(ban."_key", ':', 2) = SPLIT_PART(reason."_key", ':', 2)
-              AND ban."value_numeric" = reason."value_numeric"
- WHERE (ban."_key" IS NULL
-        OR (ban."_key" SIMILAR TO 'uid:[0-9]+:bans'
-       AND ban."type" = 'zset'))
-   AND (reason."_key" IS NULL
-        OR (reason."_key" SIMILAR TO 'banned:[0-9]+:reasons'
-       AND reason."type" = 'zset'));
+       COALESCE(bans."uid", reasons."uid"),
+       COALESCE(bans."banned_at", reasons."banned_at"),
+       bans."expires_at",
+       reasons."reason"
+  FROM (SELECT SPLIT_PART("_key", ':', 2)::BIGINT "uid",
+               TO_TIMESTAMP("value_numeric" / 1000) "banned_at",
+               CASE WHEN "unique_string" = '0'
+                    THEN NULL
+                    WHEN "unique_string"::NUMERIC < "value_numeric"
+                    THEN TO_TIMESTAMP(("value_numeric" + 1) / 1000)
+                    WHEN ("unique_string"::NUMERIC - "value_numeric") > EXTRACT(EPOCH FROM INTERVAL '100 years') * 1000
+                    THEN NULL
+                    ELSE TO_TIMESTAMP("unique_string"::NUMERIC / 1000)
+                     END "expires_at"
+          FROM "classify"."unclassified"
+         WHERE "_key" SIMILAR TO 'uid:[0-9]+:bans'
+           AND "type" = 'zset') bans
+  FULL OUTER JOIN (
+        SELECT SPLIT_PART("_key", ':', 2)::BIGINT "uid",
+               TO_TIMESTAMP("value_numeric" / 1000) "banned_at",
+               NULLIF("unique_string", '') "reason"
+          FROM "classify"."unclassified"
+         WHERE "_key" SIMILAR TO 'banned:[0-9]+:reasons'
+           AND "type" = 'zset') reasons
+    ON bans."uid" = reasons."uid"
+   AND bans."banned_at" = reasons."banned_at"
+ ORDER BY COALESCE(bans."banned_at", reasons."banned_at") ASC;
 
 ALTER TABLE "classify"."user_bans"
 	ADD PRIMARY KEY ("ban_id"),
