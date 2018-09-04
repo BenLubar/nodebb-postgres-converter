7262fc01046858bd6eb25336255abce86dd4e6e4
Ben Lubar <ben.lubar@gmail.com>
2018-09-03 21:57:57 -0500

    Fix email:sorted and username:sorted zset deletion.

--- 0006.delete.users.emailsorted.sql
+++ 0006.delete.users.emailsorted.sql
@@ -2,5 +2,5 @@ DELETE FROM "classify"."unclassified" uc
  USING "classify"."users" u
  WHERE uc."_key" = 'email:sorted'
    AND uc."type" = 'zset'
-   AND uc."unique_string" = u."email"
+   AND uc."unique_string" = u."email" || ':' || u."uid"
    AND uc."value_numeric" = 0;
--- 0006.delete.users.usernamesorted.sql
+++ 0006.delete.users.usernamesorted.sql
@@ -2,5 +2,5 @@ DELETE FROM "classify"."unclassified" uc
  USING "classify"."users" u
  WHERE uc."_key" = 'username:sorted'
    AND uc."type" = 'zset'
-   AND uc."unique_string" = u."username"
+   AND uc."unique_string" = u."userslug" || ':' || u."uid"
    AND uc."value_numeric" = 0;
