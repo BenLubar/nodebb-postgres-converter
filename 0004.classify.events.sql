8978c78d2784ee47e3968e04473fb51a141b36d3
Ben Lubar <ben.lubar@gmail.com>
2018-09-04 16:41:20 -0500

    Split more fields out of event data.

--- 0004.classify.events.sql
+++ 0004.classify.events.sql
@@ -12,6 +12,11 @@ CREATE TABLE "classify"."events" (
 	"timestamp" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	"ip" INET NOT NULL,
 	"uid" BIGINT NOT NULL,
+	"targetUid" BIGINT,
+	"pid" BIGINT,
+	"tid" BIGINT,
+	"oldValue" TEXT COLLATE "C",
+	"newValue" TEXT COLLATE "C",
 	"data" JSONB NOT NULL DEFAULT '{}'
 ) WITHOUT OIDS;
 
@@ -21,10 +26,16 @@ SELECT eid."unique_string"::BIGINT,
        TO_TIMESTAMP(eid."value_numeric" / 1000),
        SPLIT_PART(ip."value", ',', 1)::INET,
        uid."value"::BIGINT,
+       targetUid."value"::BIGINT,
+       pid."value"::BIGINT,
+       tid."value"::BIGINT,
+       oldValue."value",
+       newValue."value",
        COALESCE((SELECT jsonb_object_agg(data."field", data."value")
                    FROM "event_data" data
                   WHERE data."eid" = eid."unique_string"::BIGINT
-                    AND data."field" NOT IN ('eid', 'type', 'timestamp', 'ip', 'uid')), '{}')
+                    AND data."field" NOT IN ('eid', 'type', 'timestamp', 'ip', 'uid', 'targetUid', 'pid', 'tid')
+                    AND data."field" NOT SIMILAR TO '(old|new)(Email|Title|Username)'), '{}')
   FROM "classify"."unclassified" eid
  INNER JOIN "event_data" type
          ON type."eid" = eid."unique_string"::BIGINT
@@ -35,6 +46,21 @@ SELECT eid."unique_string"::BIGINT,
  INNER JOIN "event_data" uid
          ON uid."eid" = eid."unique_string"::BIGINT
         AND uid."field" = 'uid'
+  LEFT JOIN "event_data" targetUid
+         ON targetUid."eid" = eid."unique_string"::BIGINT
+        AND targetUid."field" = 'targetUid'
+  LEFT JOIN "event_data" pid
+         ON pid."eid" = eid."unique_string"::BIGINT
+        AND pid."field" = 'pid'
+  LEFT JOIN "event_data" tid
+         ON tid."eid" = eid."unique_string"::BIGINT
+        AND tid."field" = 'tid'
+  LEFT JOIN "event_data" oldValue
+         ON oldValue."eid" = eid."unique_string"::BIGINT
+        AND oldValue."field" IN ('oldEmail', 'oldTitle', 'oldUsername')
+  LEFT JOIN "event_data" newValue
+         ON newValue."eid" = eid."unique_string"::BIGINT
+        AND newValue."field" IN ('newEmail', 'newTitle', 'newUsername')
  WHERE eid."_key" = 'events:time'
    AND eid."type" = 'zset';
 
@@ -54,3 +80,6 @@ CREATE INDEX ON "classify"."events"("type");
 CREATE INDEX ON "classify"."events"("timestamp");
 CREATE INDEX ON "classify"."events"("ip");
 CREATE INDEX ON "classify"."events"("uid");
+CREATE INDEX ON "classify"."events"("targetUid");
+CREATE INDEX ON "classify"."events"("pid");
+CREATE INDEX ON "classify"."events"("tid");
