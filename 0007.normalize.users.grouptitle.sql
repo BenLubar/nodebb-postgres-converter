e6f578da3ec30bdb6fb9713455ece6e1c63e6910
Ben Lubar <ben.lubar@gmail.com>
2018-09-04 21:56:30 -0500

    Fix normalize.users.grouptitle

--- 0007.normalize.users.grouptitle.sql
+++ 0007.normalize.users.grouptitle.sql
@@ -1,5 +1,11 @@
 ALTER TABLE "classify"."users"
-	ALTER "groupTitle" TYPE BIGINT
-	USING (SELECT g."gid"
-	         FROM "classify"."groups" g
-	        WHERE g."name" = "groupTitle");
+	ADD "groupTitle_gid" BIGINT;
+
+UPDATE "classify"."users" u
+   SET "groupTitle_gid" = g."gid"
+  FROM "classify"."groups" g
+ WHERE g."name" = u."groupTitle";
+
+ALTER TABLE "classify"."users"
+	ALTER "groupTitle" TYPE BIGINT USING "groupTitle_gid",
+	DROP "groupTitle_gid";
